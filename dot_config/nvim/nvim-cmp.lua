114b9338d81b93f2eaa7da66fb2ab80bbd0f5599
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2022-12-03 21:17:58 +0100

    Added snippet support for completion

--- dot_config/nvim/nvim-cmp.lua
+++ dot_config/nvim/nvim-cmp.lua
@@ -66,4 +66,19 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['pyright'].setup {
 capabilities = capabilities
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['pyright'].setup {
+capabilities = capabilities
+}
+require('lspconfig')['clangd'].setup
+{
+    capabilities = capabilities;
+}
+require('lspconfig')['clangd'].setup
+{
+    capabilities = capabilities;
+}
 }
