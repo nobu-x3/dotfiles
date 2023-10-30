3e7d6f47e0421eac4c2e7a494829f6ef423695c1
Dominik <dominik.kurasbediani@gmail.com>
2023-10-30 22:09:28 +0100

    Added lldb to packages to install

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,5 +1,5 @@
 # general deps
-sudo pacman -S git neovim alacritty picom feh gtk3 htop pulseaudio dunst polybar neofetch xclip llvm-libs=16.0.6-1 llvm=16.0.6-1 clang=16.0.6-1 cmake=3.27.7-2 zsh=5.9-4 ripgrep=13.0.0-3 fzf=0.43.0-1 tmux=3.3_a-7 curl amdvlk keychain renderdoc glslang vulkan-validation-layers=1.3.268.0-1 --noconfirm
+sudo pacman -S git neovim alacritty picom feh gtk3 htop pulseaudio dunst polybar neofetch xclip llvm-libs=16.0.6-1 llvm=16.0.6-1 clang=16.0.6-1 cmake=3.27.7-2 zsh=5.9-4 ripgrep=13.0.0-3 fzf=0.43.0-1 tmux=3.3_a-7 curl amdvlk keychain renderdoc glslang vulkan-validation-layers=1.3.268.0-1 lldb --noconfirm
 yay -S nvim-packer-git obsidian-bin slack-desktop  --noconfirm
 
 # optional stuff
