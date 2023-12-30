b101391ac11808c8d1ffe4411d4839444777e0fc
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-12-30 18:52:56 +0100

    Fixed alacritty deprecation stuff, install scripts

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,2 +1,2 @@
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
