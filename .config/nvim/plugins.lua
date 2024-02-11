30262758429deb726d75081ff636adaad3976df0
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2024-02-11 21:40:53 +0100

    Added p4.nvim setup command

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -112,3 +112,5 @@ vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual(
 vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
     desc = "Search on current file"
 })
+
+require("p4-nvim").setup()
