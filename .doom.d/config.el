fb60328dd071f93719be10d5c39390f0eb1d9577
Dominik <dominik.kurasbediani@gmail.com>
2024-07-30 16:25:04 +0200

    Updated doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,6 +6,42 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-tokyo-night)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
 (setq user-full-name "Dominik Kurasbediani"
       user-mail-address "dominik.kurasbediani@gmail.com"
       projectile-project-search-path '("~/workspace/")
@@ -20,78 +56,6 @@
 (after! lsp-clangd (set-lsp-priority! 'clangd 2))
 (setq +format-with-lsp nil)
 (setq clang-format-style "file")
-
-(setq dap-auto-configure-mode t)
-(require 'dap-cpptools)
-
-(map! :map dap-mode-map
-      :leader
-      :prefix ("d" . "dap")
-      ;; basics
-      :desc "dap next"          "n" #'dap-next
-      :desc "dap step in"       "i" #'dap-step-in
-      :desc "dap step out"      "o" #'dap-step-out
-      :desc "dap continue"      "c" #'dap-continue
-      :desc "dap hydra"         "h" #'dap-hydra
-      :desc "dap debug restart" "r" #'dap-debug-restart
-      :desc "dap debug"         "s" #'dap-debug
-
-      ;; debug
-      :prefix ("dd" . "Debug")
-      :desc "dap debug recent"  "r" #'dap-debug-recent
-      :desc "dap debug last"    "l" #'dap-debug-last
-
-      ;; eval
-      :prefix ("de" . "Eval")
-      :desc "eval"                "e" #'dap-eval
-      :desc "eval region"         "r" #'dap-eval-region
-      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
-      :desc "add expression"      "a" #'dap-ui-expressions-add
-      :desc "remove expression"   "d" #'dap-ui-expressions-remove
-
-      :prefix ("db" . "Breakpoint")
-      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
-      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
-      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
-      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
-
-
-
-
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-(setq doom-font (font-spec :family "Fira Code" :size 16)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -124,30 +88,32 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
 (map! :leader "r n" #'lsp-rename)
 (map! :n "] d" #'next-error)
 (map! :n "[ d" #'previous-error)
 (map! :n "g h" #'lsp-clangd-find-other-file)
-(map! :n "g t" nil)
-(map! :n "g T" nil)
 (map! :n "g t" #'next-buffer)
 (map! :n "g T" #'previous-buffer)
 (map! :n "g d" #'+lookup/definition)
 (map! :n "g r" #'+lookup/references)
 (map! :n "f f" #'find-file)
-(map! :n "}" nil)
-(map! :n "{" nil)
-(map! :n "}" #'c-end-of-defun)
-(map! :n "{" #'c-beginning-of-defun)
 (map! :n "C-s" #'lsp-ui-imenu)
 (map! :n "C-e" #'lsp-ui-flycheck-list)
 (map! :n "C-a" #'lsp-execute-code-action)
 (map! :n "f g" nil)
-(map! :n "f g" #'consult-grep)
+(map! :n "f g" #'+default/search-project)
+(map! :v "f g" #'+default/search-project-for-symbol-at-point)
 (map! :n "C-n" nil)
 (map! :n "C-n" #'+treemacs/toggle)
 (map! :n "C-q" #'kill-current-buffer)
+(map! :leader "h p" #'doom/open-private-config)
+(map! :leader "f" #'lsp-format-buffer)
+(map! :leader "a" #'lsp-execute-code-action)
+(map! :leader "s p" #'projectile-switch-project)
+(map! :leader "b r" #'ibuffer-update)
+(map! :leader "d d" #'dired-create-directory)
+(map! :leader "d f" #'dired-create-empty-file)
+(map! :leader "c c" #'project-compile)
 
 (use-package! tree-sitter
   :hook (prog-mode . turn-on-tree-sitter-mode)
@@ -161,22 +127,5 @@
 
 
 (setq doom-modeline-time t)
-
-
-(use-package! modern-cpp-font-lock
-  :ensure t)
-
-(after! centaur-tabs
-  :ensure t
-  :config
-  (centaur-tabs-group-by-projectile-project)
-  (centaur-tabs-enable-buffer-reordering)
-  (setq centaur-tabs-mode 'over
-        centaur-tabs-height 32
-        centaur-tabs-set-icons t
-        centaur-tabs-gray-out-icons 'buffer
-        centaur-tabs-set-bar 'over
-        centaur-tabs-set-modified-marker t
-        centaur-tabs-modifier-marker "•")
-  (centaur-tabs-mode t)
-  )
+(load! "c3-mode.el")
+(load! "jai-mode.el")
