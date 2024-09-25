bc9e00b2208b9f11b9eb701c51336a17de1b53a2
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2024-09-25 22:20:53 +0200

    Vimspector keybindings

--- .config/nvim/vimspector.lua
+++ .config/nvim/vimspector.lua
@@ -1,9 +1,9 @@
 vim.cmd([[
-nmap <F5> <cmd>call vimspector#Launch()<cr>
-nmap <F8> <cmd>call vimspector#Reset()<cr>
-nmap <F10> <cmd>call vimspector#StepOver()<cr>")
-nmap <F12> <cmd>call vimspector#StepOut()<cr>")
-nmap <F11> <cmd>call vimspector#StepInto()<cr>")
+nmap <leader>ds <cmd>call vimspector#Launch()<cr>
+nmap <leader>dr <cmd>call vimspector#Reset()<cr>
+nmap <leader>dn <cmd>call vimspector#StepOver()<cr>")
+nmap <leader>do <cmd>call vimspector#StepOut()<cr>")
+nmap <leader>di <cmd>call vimspector#StepInto()<cr>")
 nmap <leader>db <cmd>call vimspector#ToggleBreakpoint()<cr>")
 nmap <leader>dw <cmd>call vimspector#AddWatch()<cr>")
 nmap <leader>de <cmd>call vimspector#Evaluate()<cr>")
